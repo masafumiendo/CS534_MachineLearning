eb5e7c139c6d242e459082eb4c178f72812bc035
遠藤正文 <endoumasafumi@endoumasafuminoMacBook-Pro-2.local>
2019-11-24 15:26:56 -0800

    modify decisiontree class to incorpolate weights info

--- I3_Trees/part3.py
+++ I3_Trees/part3.py
@@ -15,19 +15,24 @@ class AdaBoost:
         self.num_learners = num_learners
         self.learners = [self.class_learner for _ in range(self.num_learners)]
         self.alpha = np.zeros(self.num_learners)
+        self.trees = []
 
     # Public method
     # Method for training weak learners
     def train(self, df):
 
-        x, y = df # Need to split
+        x = df.drop("Class", axis=1)
+        y = df["Class"].to_list()
+        y = self.__label_converter(y)
 
         N = len(x)
-        weight = np.ones(N) / N
+        weight = np.ones(N) / N # Initialize the weight
 
         for i in range(self.num_learners):
             tree = self.learners[i].make_tree(df)
             mistakes = (self.learners[i].predict(x, tree) != y)
+
+            self.trees.append(tree)
 
             # Compute the epsilon and alpha
             epsilon = np.sum(weight * mistakes)
@@ -35,18 +40,31 @@ class AdaBoost:
 
             # Update the weight
             weight = weight * np.exp(self.alpha[i] * mistakes)
-            weight = weight / sum(weight)
+            weight = weight / np.sum(weight)
 
     # Method for prediction
     def predict(self, df):
 
-        x, y = df # Need to split
+        x = df.drop("Class", axis=1)
 
         base_pred = np.zeros((self.num_learners, len(x)))
         for i in range(self.num_learners):
-            base_pred[i] = self.learners[i].predict()
+            base_pred[i] = self.learners[i].predict(x, self.trees[i])
 
         return np.sign(base_pred.T @ self.alpha)
+
+    def __label_converter(self, y):
+
+        num_row = len(y)
+        y_convert = np.zeros(len(y))
+
+        for i in range(num_row):
+            if y[i] == 1:
+                y_convert[i] = 1
+            elif y[i] == 0:
+                y_convert[i] = -1
+
+        return y_convert
 
 def main():
 
@@ -54,9 +72,11 @@ def main():
 
     df_train, df_valid, df_test = preprocess.get_data()
 
-    DT = DecisionTree.DecisionTree(1)
+    DT = DecisionTree.DecisionTree(1) # with max depth is one
 
-    adaboost = AdaBoost(DT, 5)
+    adaboost = AdaBoost(DT, 10) # The number of weak leaners
+
+    adaboost.train(df_train)
 
 if __name__ == '__main__':
     main()
