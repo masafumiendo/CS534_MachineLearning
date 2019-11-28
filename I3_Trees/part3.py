eb316355c0ab56ab9e231ed7d75b086cef5a87e3
遠藤正文 <endoumasafumi@endoumasafuminoMacBook-Pro-2.local>
2019-11-27 20:09:09 -0800

    modify alpha for adaboost class

--- I3_Trees/part3.py
+++ I3_Trees/part3.py
@@ -14,7 +14,6 @@ class AdaBoost:
         self.class_learner = class_learner
         self.num_learners = num_learners
         self.learners = [self.class_learner for _ in range(self.num_learners)]
-        self.alpha = np.zeros(self.num_learners)
         self.trees = []
 
     # Public method
@@ -23,17 +22,17 @@ class AdaBoost:
 
         x = df.drop("Class", axis=1)
         y = df["Class"].to_list()
-        # y = self.__label_converter(y)
 
         N = len(x)
         weight = np.ones(N) / N # Initialize the weight
         df['weight'] = weight
+        alpha = np.zeros(self.num_learners)
 
         for i in range(self.num_learners):
 
             tree = self.learners[i].make_tree_adaboost(df)
-            print(weight)
             mistakes = np.zeros(len(df))
+
             for j, example in enumerate(df.iterrows()):
                 df_ex = df[df.index == j].drop(["Class", "weight"], axis=1)
                 prediction = self.learners[i].predict(df_ex, tree)
@@ -46,30 +45,27 @@ class AdaBoost:
 
             # Compute the epsilon and alpha
             epsilon = np.sum(weight * mistakes)
-            print("epsilon", epsilon)
-            self.alpha[i] = 1/2 * np.log((1.0 - epsilon)/epsilon)
+
+            alpha[i] = 1/2 * np.log((1.0 - epsilon)/epsilon)
 
             # Update the weight
             weight = df["weight"].values
             for j in range(len(weight)):
                 if mistakes[j] == True:
-                    # print(j)
-                    weight[j] = weight[j] * np.exp(self.alpha[i])
+                    weight[j] = weight[j] * np.exp(alpha[i])
                 elif mistakes[j] == False:
-                    weight[j] = weight[j] * np.exp(-self.alpha[i])
-            print("n mistakes", sum(mistakes))
+                    weight[j] = weight[j] * np.exp(-alpha[i])
 
             weight = weight / np.sum(weight)
             df['weight'] = weight
-            # print(df.columns)
-            #print(sum(weight))
-            print("alpha ", self.alpha)
-           # print(np.unique(weight))
 
-    # Method for prediction
-    def predict(self, df):
+        return alpha
+
+    # Method for prediction by a strong learner
+    def predict(self, df, alpha):
 
         base_pred = np.zeros((self.num_learners, len(df)))
+
         for i in range(self.num_learners):
             for j, example in enumerate(df.iterrows()):
                 df_ex = df[df.index == j].drop("Class", axis=1)
@@ -77,19 +73,24 @@ class AdaBoost:
                 if prediction == 0:
                     prediction = -1
                 base_pred[i][j] = prediction
-            #print(base_pred[i])
-            print(np.sign(base_pred.T @ self.alpha))
-        self.alpha = self.alpha / np.sum(self.alpha)
-        print(np.sign(base_pred.T @ self.alpha))
-        return np.sign(base_pred.T @ self.alpha)
+
+            print("alpha: {0}".format(alpha))
+            print("prediction of weak leaner {0}: {1}".format(i, base_pred[i][0:10]))
+
+        alpha = alpha / np.sum(alpha)
+        print(base_pred.shape)
+        voted_pred = np.sign(base_pred.T @ alpha)
+
+        print("prediction of strong leaner {0}".format(voted_pred[0:10]))
+
+        return voted_pred
 
     # Method for computing accuracy
     def accuracy(self, prediction, df):
 
         y = df["Class"].to_list()
         y = self.__label_converter(y)
-       # prediction = self.__label_converter(prediction)
-        # print(self.alpha)
+
         cnt = 0
 
         for i in range(len(y)):
@@ -98,7 +99,6 @@ class AdaBoost:
             else:
                 pass
 
-        print("count", cnt)
         accuracy = cnt / len(y)
 
         return accuracy
@@ -132,43 +132,35 @@ def learners_plot(train, valid, n_learners):
     
     plt.clf()
 
-if __name__ == '__main__':
+def main():
 
     preprocess = PreProcess.PreProcess()
 
     df_train, df_valid, df_test = preprocess.get_data()
 
-    # DT = DecisionTree.DecisionTree(2) # with max depth is one
-    #
-    # adaboost = AdaBoost(DT, 5) # The number of weak leaners
-    #
-    # adaboost.train(df_train)
-    #
-    # train_pred = adaboost.predict(df_train)
-    # print(train_pred)
-    # train_acc = adaboost.accuracy(train_pred, df_train)
-    #
-    # valid_predict = adaboost.predict(df_valid)
-    # accuracy = adaboost.accuracy(valid_predict, df_valid)
-    #
-    # print("valid acc", accuracy)
-    # print("train acc:", train_acc)
-
-    n_learners = [1, 2, 5, 10, 15]
+    n_learners = [1, 2]
     train_accs = []
     valid_accs = []
+
     for n_learner in n_learners:
+        print("")
         DT = DecisionTree.DecisionTree(1)  # with max depth is one
         adaboost = AdaBoost(DT, n_learner)  # The number of weak leaners
-        adaboost.train(df_train)
+        alpha = adaboost.train(df_train)
 
-        train_pred = adaboost.predict(df_train)
-        valid_pred = adaboost.predict(df_valid)
+        train_pred = adaboost.predict(df_train, alpha)
+        valid_pred = adaboost.predict(df_valid, alpha)
 
         train_acc = adaboost.accuracy(train_pred, df_train)
         valid_acc = adaboost.accuracy(valid_pred, df_valid)
 
+        print("training accuracy: {0}, validation accuracy: {1} for num. of leaners of {2}".format(train_acc, valid_acc, n_learner))
+        print("")
         train_accs.append(train_acc)
         valid_accs.append(valid_acc)
 
     learners_plot(100*np.array(train_accs), 100*np.array(valid_accs), n_learners)
+
+if __name__ == '__main__':
+
+    main()
